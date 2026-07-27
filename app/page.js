bd8d774cbbd75b3d4f2c8a65ffa4f89cc166f2c9
dutaposco <dutaposco@gmail.com>
2026-07-27 08:45:36 +0700

    Remove Spotify button

--- app/page.js
+++ app/page.js
@@ -29,11 +29,6 @@ const socialLinks = [
     icon: '/logo/instagram.svg', 
     alt: 'Instagram',
   },
-  {
-    href: 'https://open.spotify.com/playlist/2gQgfHfdjW8S0S4Ypfu1jV',
-    icon: '/logo/spotify.svg', 
-    alt: 'Spotify',
-  },
 ]
 
 export default function HomePage() {
