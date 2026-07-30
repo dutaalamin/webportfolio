169a00c2edff620c7af7b33fe25dfe1941ff0b5d
dutaposco <dutaposco@gmail.com>
2026-07-30 10:52:13 +0700

    chore: disable pinch-to-zoom and double-tap zoom on mobile devices

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,13 @@ export const metadata = {
   },
 }
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
 import ClickSoundProvider from './components/ClickSoundProvider'
 
 export default function RootLayout({ children }) {
